558abba5c280024551f6f43c7abd58ff02626417
WAKE-UP-TO-REALITY <asishkumar54035@gmail.com>
2025-03-29 10:49:53 +0530

    adding base of django
    ...

--- ai_post_generator/posts/views.py
+++ ai_post_generator/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
+import os
+import json
+import requests
+import logging
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from dotenv import load_dotenv
+from django.conf import settings
 # Create your views here.
+
+
+load_dotenv()
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
+
+def home(request):
+    return render(request, 'posts/index.html')
